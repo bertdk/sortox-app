0471fa4c4138c54513d0cc392f20b465ea56d23b
bertdk <bertdk10@gmail.com>
2021-01-31 23:57:40 +0100

    feat: basic controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import React from "react";
-import "./App.css";
-import { List } from "./Components/List/List.component";
+import React, { useEffect, useState } from 'react'
+import { selectionSort } from './algorithms/sort/selection'
+import './App.css'
+import { Controls } from './Components/List/Controls.component'
+import { List } from './Components/List/List.component'
+import { randomNumber } from './utils/randomNumber'
 
-function App() {
+export const App = () => {
+  const [list, setList] = useState([] as number[])
+  const [length, setLength] = useState(10)
+
+  useEffect(() => {
+    setList(createRandomList(length))
+  }, [length])
+
+  const onSort = () => {
+    selectionSort(list, setList)
+  }
+
+  const onRandomize = () => {
+    setList(createRandomList(length))
+  }
+
+  const createRandomList = (length: number) => {
+    const list: number[] = []
+    for (let i = 1; i <= length; i++) {
+      let newNumber = randomNumber(length)
+      // eslint-disable-next-line no-loop-func
+      while (list.some((n) => n === newNumber)) {
+        newNumber = randomNumber(length)
+      }
+      list.push(newNumber)
+    }
+    return list
+  }
+
+  const onSlide = (e: any, value: number | number[]) => {
+    setLength(value as number)
+  }
+
   return (
-    <div>
-      <List />
-    </div>
-  );
+    <>
+      <Controls onSort={onSort} onRandomize={onRandomize} onSlide={onSlide} />
+      <List list={list} />
+    </>
+  )
 }
-
-export default App;
